cd689f8dc5088e837d695810fc36535045dbb1d7
whjr1750 <91913630+whjr1750@users.noreply.github.com>
2023-02-13 16:42:00 +0530

    Update addMarkers.js

--- js/addMarkers.js
+++ js/addMarkers.js
@@ -1,9 +1,9 @@
 AFRAME.registerComponent("create-markers", {
   init: async function () {
-    let mainScene = document.querySelector("#main-scene");
-    let dishes = await this.getDishes();
+    var mainScene = document.querySelector("#main-scene");
+    var dishes = await this.getDishes();
     dishes.map(dish => {
-      let marker = document.createElement("a-marker");
+      var marker = document.createElement("a-marker");
       marker.setAttribute("id", dish.id);
       marker.setAttribute("type", "pattern");
       marker.setAttribute("url", dish.marker_pattern_url);
@@ -13,108 +13,93 @@ AFRAME.registerComponent("create-markers", {
       marker.setAttribute("marker-handler", {});
       mainScene.appendChild(marker);
 
-      let model = document.createElement("a-entity");
-      model.setAttribute("id", `model-${dish.id}`);
-      model.setAttribute("position", dish.model_geometry.position);
-      model.setAttribute("rotation", dish.model_geometry.rotation);
-      model.setAttribute("scale", dish.model_geometry.scale);
-      model.setAttribute("gltf-model", `url(${dish.model_url})`);
-      model.setAttribute("gesture-handler", {});
-      model.setAttribute("visible", false);
-      marker.appendChild(model);
+      // Getting today's day
+      var todaysDate = new Date();
+      var todaysDay = todaysDate.getDay();
+      // Sunday - Saturday : 0 - 6
+      var days = [
+        "sunday",
+        "monday",
+        "tuesday",
+        "wednesday",
+        "thursday",
+        "friday",
+        "saturday"
+      ];
 
-      let mainplane = document.createElement("a-plane");
-      mainplane.setAttribute("id", `main-plane-${dish.id}`);
-      mainplane.setAttribute("position", {
-        x: 0,
-        y: 0,
-        z: 0
-      });
-      mainplane.setAttribute("rotation", {
-        x: -90,
-        y: 0,
-        z: 0
-      });
-      mainplane.setAttribute("width", 1.7);
-      mainplane.setAttribute("height", 1.5);
-      marker.appendChild(mainplane);
+      
+        // Adding 3D model to scene
+        var model = document.createElement("a-entity");
+        model.setAttribute("id", `model-${dish.id}`);
+        model.setAttribute("position", dish.model_geometry.position);
+        model.setAttribute("rotation", dish.model_geometry.rotation);
+        model.setAttribute("scale", dish.model_geometry.scale);
+        model.setAttribute("gltf-model", `url(${dish.model_url})`);
+        model.setAttribute("gesture-handler", {});
+        model.setAttribute("visible", false);
+        marker.appendChild(model);
 
-      let titleplane = document.createElement("a-plane");
-      titleplane.setAttribute("id", `title-plane-${dish.id}`);
-      titleplane.setAttribute("position", {
-        x: 0,
-        y: 0.89,
-        z: 0.02
-      });
-      titleplane.setAttribute("rotation", {
-        x: 0,
-        y: 0,
-        z: 0
-      });
-      titleplane.setAttribute("width", 1.69);
-      titleplane.setAttribute("height", 0.3);
-      titleplane.setAttribute("material", {
-        color: "yellow"
-      })
-      titleplane.setAttribute("visible", false);
-      mainplane.appendChild(titleplane);
+        // Ingredients Container
+        var mainPlane = document.createElement("a-plane");
+        mainPlane.setAttribute("id", `main-plane-${dish.id}`);
+        mainPlane.setAttribute("position", { x: 0, y: 0, z: 0 });
+        mainPlane.setAttribute("rotation", { x: -90, y: 0, z: 0 });
+        mainPlane.setAttribute("width", 1.7);
+        mainPlane.setAttribute("height", 1.5);
+        mainPlane.setAttribute("visible", false);
+        marker.appendChild(mainPlane);
 
-      let dishtitle = document.createElement("a-entity");
-      dishtitle.setAttribute("id", `dish-title-${dish.id}`);
-      dishtitle.setAttribute("position", {
-        x: 0,
-        y: 0,
-        z: 0.1
-      });
-      dishtitle.setAttribute("rotation", {
-        x: 0,
-        y: 0,
-        z: 0
-      });
-      dishtitle.setAttribute("text", {
-        font: "monoid",
-        color: "black",
-        width: 1.8,
-        height: 1,
-        align: "center",
-        value: dish.dish_name.toUpperCase()
-      });
-      dishtitle.setAttribute("visible", false);
-      titleplane.appendChild(dishtitle);
+        // Dish title background plane
+        var titlePlane = document.createElement("a-plane");
+        titlePlane.setAttribute("id", `title-plane-${dish.id}`);
+        titlePlane.setAttribute("position", { x: 0, y: 0.89, z: 0.02 });
+        titlePlane.setAttribute("rotation", { x: 0, y: 0, z: 0 });
+        titlePlane.setAttribute("width", 1.69);
+        titlePlane.setAttribute("height", 0.3);
+        titlePlane.setAttribute("material", { color: "#F0C30F" });
+        mainPlane.appendChild(titlePlane);
 
-      let ingredients = document.createElement("a-entity");
-      ingredients.setAttribute("id", `ingredients-${dish.id}`);
-      ingredients.setAttribute("position", {
-        x: 0.3,
-        y: 0,
-        z: 0.1
-      });
-      ingredients.setAttribute("rotation", {
-        x: 0,
-        y: 0,
-        z: 0
-      });
-      ingredients.setAttribute("text", {
-        font: "monoid",
-        color: "black",
-        width: 2,
-        align: "left",
-        value: `${dish.ingredients.join("\n\n")}`
-      });
-      ingredients.setAttribute("visible", false);
-      mainplane.appendChild(ingredients);
+        // Dish title
+        var dishTitle = document.createElement("a-entity");
+        dishTitle.setAttribute("id", `dish-title-${dish.id}`);
+        dishTitle.setAttribute("position", { x: 0, y: 0, z: 0.1 });
+        dishTitle.setAttribute("rotation", { x: 0, y: 0, z: 0 });
+        dishTitle.setAttribute("text", {
+          font: "monoid",
+          color: "black",
+          width: 1.8,
+          height: 1,
+          align: "center",
+          value: dish.dish_name.toUpperCase()
+        });
+        titlePlane.appendChild(dishTitle);
 
-      var pricePlane = document.createElement("a-image");
+        // Ingredients List
+        var ingredients = document.createElement("a-entity");
+        ingredients.setAttribute("id", `ingredients-${dish.id}`);
+        ingredients.setAttribute("position", { x: 0.3, y: 0, z: 0.1 });
+        ingredients.setAttribute("rotation", { x: 0, y: 0, z: 0 });
+        ingredients.setAttribute("text", {
+          font: "monoid",
+          color: "black",
+          width: 2,
+          align: "left",
+          value: `${dish.ingredients.join("\n\n")}`
+        });
+        mainPlane.appendChild(ingredients);
+
+        //Plane to show the price of the dish
+        var pricePlane = document.createElement("a-image");
         pricePlane.setAttribute("id", `price-plane-${dish.id}`);
         pricePlane.setAttribute(
-          "src",
-          "https://raw.githubusercontent.com/whitehatjr/menu-card-app/main/black-circle.png"
+          "src", "https://raw.githubusercontent.com/whitehatjr/menu-card-app/main/black-circle.png"
         );
         pricePlane.setAttribute("width", 0.8);
         pricePlane.setAttribute("height", 0.8);
         pricePlane.setAttribute("position", { x: -1.3, y: 0, z: 0.3 });
         pricePlane.setAttribute("rotation", { x: -90, y: 0, z: 0 });
         pricePlane.setAttribute("visible", false);
+
         //Price of the dish
         var price = document.createElement("a-entity");
         price.setAttribute("id", `price-${dish.id}`);
@@ -127,13 +112,20 @@ AFRAME.registerComponent("create-markers", {
           align: "center",
           value: `Only\n $${dish.price}`
         });
+
+
         pricePlane.appendChild(price);
         marker.appendChild(pricePlane);
-    })
+      
+    });
   },
   getDishes: async function () {
-    return await firebase.firestore().collection("dishes").get().then(snapshot => {
-      return snapshot.docs.map(doc => doc.data())
-    })
+    return await firebase
+      .firestore()
+      .collection("dishes")
+      .get()
+      .then(snap => {
+        return snap.docs.map(doc => doc.data());
+      });
   }
 });
